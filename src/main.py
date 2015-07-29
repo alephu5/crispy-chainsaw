1fdb5bc03b90ce0bdd3e4ccb93ded24d1d4bdd3c
alephu5 <morrison.sotelo@gmail.com>
2015-07-29 12:46:55 +0000

    Added collision detection to main.py.

--- src/main.py
+++ src/main.py
@@ -2,18 +2,21 @@
 
 import pygame
 from pygame.image import load as loadImage
-from pygame.locals import *
+from pygame.locals import (K_LEFT, K_RIGHT, K_UP, K_DOWN,
+                           K_ESCAPE, QUIT, KEYDOWN, KEYUP)
 from pytmx.util_pygame import load_pygame
 import pyscroll
 import os
 import string
 import itertools
 
-FRAME_RATE = 30
-
+FRAME_RATE = 90
+IMAGE_RATE = 45
 CHAR_PATH = '../assets/Chars/'
 ANIMS = ['Hurt', 'Shoot', 'Slash', 'Spellcast', 'Thrust', 'Walk']
 INIT_IMG = os.path.join('Walk/Down/tile_130.png')
+SCREEN_SIZE = 1024, 768
+MAP_PATH = '../assets/base.tmx'
 
 
 def sortImages(imageSet):
@@ -50,12 +53,13 @@ class Character(pygame.sprite.Sprite):
         self.health = 100
         self.direction = None
         self.speed = 300/1000
-        self.unobstructed = True
+        self.obstructed = False
         baseimg = os.path.join(CHAR_PATH, name, INIT_IMG)
         anims = loadAnims(name)
         self.image = loadImage(baseimg)
-        self.blockers = []
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
+        # Load images for animation.
         self.walk = {}
         self.walk[K_RIGHT] = itertools.cycle(anims['Walk']['Right'])
         self.walk[K_LEFT] = itertools.cycle(anims['Walk']['Left'])
@@ -63,6 +67,8 @@ class Character(pygame.sprite.Sprite):
         self.walk[K_UP] = itertools.cycle(anims['Walk']['Up'])
         self.walk[None] = itertools.cycle([self.image])
         self.images = self.walk[None]
+        self.imageclock = 0  # Tracks how long an image is on the screen.
+        self.imagerate = IMAGE_RATE  # Images per second.
 
     def changeDirection(self, newDir):
         "Change the image set to animate when the direction changes."
@@ -70,20 +76,25 @@ class Character(pygame.sprite.Sprite):
             if newDir in self.walk.keys():
                 self.images = self.walk[newDir]
                 self.direction = newDir
+                self.obstructed = False
+                self.imageclock = 0
             else:
                 self.images = self.walk[None]
                 self.direction = None
 
     def update(self, deltaT):
         x, y = self.position
-        if self.direction == K_RIGHT:
-            x += self.speed * deltaT * self.unobstructed
-        if self.direction == K_LEFT:
-            x -= self.speed * deltaT * self.unobstructed
-        if self.direction == K_UP:
-            y -= self.speed * deltaT * self.unobstructed
-        if self.direction == K_DOWN:
-            y += self.speed * deltaT * self.unobstructed
+        if not self.obstructed:
+            if self.direction == K_RIGHT:
+                x += self.speed * deltaT
+            if self.direction == K_LEFT:
+                x -= self.speed * deltaT
+            if self.direction == K_UP:
+                y -= self.speed * deltaT
+            if self.direction == K_DOWN:
+                y += self.speed * deltaT
+        self.imageclock += deltaT
+        if self.imageclock >=
         image = self.images.__next__()
         self.image = image
         self.position = (x, y)
@@ -91,53 +102,90 @@ class Character(pygame.sprite.Sprite):
         self.rect.center = self.position
 
 
-def main():
-    screen_size = 1024, 768
-    screen = pygame.display.set_mode(screen_size)
-    rect = screen.get_rect()
-    dave = Character('Dave', rect.center)
-    char_group = pygame.sprite.RenderPlain(dave)
-    clock = pygame.time.Clock()
-    tiled_map = load_pygame('../assets/base.tmx')
-# Load all tiles from the foreground layer and create collision
-# rectangles.
-    blockers = pygame.sprite.Group()
-    for tile in tiled_map.layers[1].tiles():
-        x, y, img = tile
-        block = pygame.sprite.Sprite()
-        block.rect = img.get_rect()
-        block.rect.center = (x, y)
-        blockers.add(block)
-    print(len(blockers))
-    map_data = pyscroll.TiledMapData(tiled_map)
-    map_layer = pyscroll.BufferedRenderer(
-        map_data,
-        screen_size,
-        clamp_camera=True)
-    group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
-    group.add(dave)
-    group.center(dave.position)
-    group.draw(screen)
-    running = True
-    while running:
-        deltaT = clock.tick(FRAME_RATE)
+class Game:
+
+    def __init__(self, screen_size, map):
+        pygame.init
+        self.screen_size = screen_size
+        self.screen = pygame.display.set_mode(screen_size)
+        self.rect = self.screen.get_rect()
+        self.clock = pygame.time.Clock()
+        self.map = load_pygame(map)
+        self.running = False
+        self.group = pyscroll.PyscrollGroup()
+        self.obstacles = pygame.sprite.Group()
+        # Use pyscroll to ensure that the camera tracks the player and does
+        # not go off the edge.
+        map_data = pyscroll.TiledMapData(self.map)
+        map_layer = pyscroll.BufferedRenderer(
+            map_data,
+            screen_size,
+            clamp_camera=True)
+        self.group = pyscroll.PyscrollGroup(
+            map_layer=map_layer,
+            default_layer=1)
+
+    def load_obstacles(self):
+        """Assumes that all tiles in the foreground block motion. Draws
+        rectangles around each tile and creates a collision map."""
+        for tile in self.map.layers[1].tiles():
+            x, y, img = tile
+            obstacle = pygame.sprite.Sprite()
+            obstacle.rect = img.get_rect()
+            # Multiply by 32 because the tiles are 32x32 across.
+            obstacle.rect.center = (x*32, y*32)
+            obstacle.mask = pygame.mask.from_surface(img)
+            self.obstacles.add(obstacle)
+
+    def addPlayer(self, character):
+        self.player = character
+        self.group.add(character)
+        self.group.center(character.position)
+
+    def update(self, deltaT):
+        # Check for collision and block motion if true. When the direction
+        # changes, allow the player to take one step before checking
+        # for collision.
+        if pygame.sprite.spritecollideany(self.player, self.obstacles,
+                                          collided=pygame.sprite.collide_mask):
+            self.player.obstructed = True
+
+        # Check for input and simulate motion.
         for event in pygame.event.get():
             pygame.event.get()
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
-                    running = False
+                    # Kill the game if the escape key is pressed.
+                    self.running = False
                 else:
-                    dave.changeDirection(event.key)
+                    # Move in the given direction when key is presed.
+                    self.player.changeDirection(event.key)
             elif event.type == KEYUP:
-                dave.changeDirection(None)
+                # Stop moving when the key is released.
+                self.player.changeDirection(None)
             elif event.type == QUIT:
-                running = False
-        print (pygame.sprite.spritecollideany(dave, blockers))
+                self.running = False
+        # Test for collisions.
 
-        group.center(dave.rect.center)
-        group.update(deltaT)
-        group.draw(screen)
+        self.group.center(self.player.position)
+        self.group.update(deltaT)
+        self.group.draw(self.screen)
         pygame.display.update()
+
+    def start(self, frame_rate):
+        self.group.draw(self.screen)
+        self.running = True
+        while self.running:
+            deltaT = self.clock.tick(frame_rate)
+            self.update(deltaT)
+
+
+def main():
+    new_game = Game(SCREEN_SIZE, MAP_PATH)
+    dave = Character('Dave', new_game.rect.center)
+    new_game.load_obstacles()
+    new_game.addPlayer(dave)
+    new_game.start(FRAME_RATE)
 
 if __name__ == '__main__':
     main()
